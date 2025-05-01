d9b9e4968dd0c0f7fc694370176687f55e6c0af3
azen04 <anez2204@gmail.com>
2025-05-01 14:30:51 -0500

    created function and looping algo for selecting words and letters within an array

--- titleCase.js
+++ titleCase.js
@@ -1,1 +1,40 @@
 // start of Title Case: Codewars Algo
+
+// input: string with words to modify && string with words to avoid modifying
+// output: modified string
+
+// split string based on a unanimous seperator
+// push words into their own respective arrays
+// loop through the titleWordsArray and do the following checks
+// if current word is not in minorWordsArray then select index 0 and change to uppercase letter if not already uppercase
+
+// index to loop over words
+// index to 
+// an array for each string
+
+
+
+function titleCase(title, minorWords) {
+
+    let wordIndex = 0
+    let letterIndex = 0
+
+    const wordsArray = title.split(' ')
+    const minorWordsArray = minorWords.split(' ')
+
+    wordsArray
+    minorWordsArray
+
+    while (wordsArray.length > wordIndex) {
+        while (wordsArray[wordIndex].length > letterIndex) {
+
+            
+            
+            letterIndex++
+        }
+        
+        letterIndex = 0
+        wordIndex++
+    }
+    
+}
