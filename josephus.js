ce8b572a709d647bf0dd45f932689658fa7cdffa
azen04 <anez2204@gmail.com>
2025-05-02 10:05:48 -0500

    created josephus.js

---
+++ josephus.js
@@ -0,0 +1,12 @@
+// use an array to represent a circle. values in array will be selected based on passed in value, should return a new array,
+
+// input: array && number value
+// output: array
+
+// iterate array
+// iteration won't stop until array is empty
+// how far we count until we splice will depend on k
+
+function josephus(items, k) {
+
+}
