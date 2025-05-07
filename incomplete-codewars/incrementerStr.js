28d6ba3875ee44d47f9d5c957b567e93e2620d5c
azen04 <anez2204@gmail.com>
2025-05-07 10:23:56 -0500

    file system changes

---
+++ incomplete-codewars/incrementerStr.js
@@ -0,0 +1,10 @@
+// input: string
+// output: new string
+
+// check if string ends with numbers
+// select numbers at end of string and increment the enumber
+// if stirng does not end in numbers then append 1
+
+// immediate checks
+// start from end of string for number
+
